c445c4790a4843a8ea6b2295dd58724185920c11
Bulava Eduard <bulavaeduard@gmail.com>
2018-01-16 21:46:29 +0200

    infinity fix

--- src/js/index.js
+++ src/js/index.js
@@ -25,6 +25,12 @@ function onMouseMove(event) {
         let domRect = event.target.getBoundingClientRect()
         let clientX = event.type === 'touchmove' ? event.touches[0].clientX : event.clientX
         let leftSide = clientX - (domRect.left + event.target.clientLeft)
+        if (mouseDown < clientX - (domRect.left + event.target.clientLeft) && parseInt(event.target.children[0].style.left) === 0){
+            let elem = event.target.children[0].children[event.target.children[0].children.length - 1].cloneNode()
+            event.target.children[0].prepend(elem)
+            event.target.children[0].removeChild(event.target.children[0].children[event.target.children[0].children.length - 1])
+            offset[id] += -600
+        }
         event.target.children[0].style.left = offset[id] + leftSide - mouseDown + 'px'
     }
 }
@@ -34,21 +40,18 @@ function onMouseUp(event) {
         let id = event.target.children[0].id
         let clientX = event.type === 'touchend' ? event.changedTouches[0].clientX : event.clientX
         if (mouseDown > clientX - (domRect.left + event.target.clientLeft)) {
-            if ((Math.abs(parseInt(offset[id])) + 600) / 600 === event.target.children[0].children.length) {
-                event.target.children[0].style.left = 0 + 'px'
-                offset[id] = 0
+            if ((Math.abs(parseInt(offset[id])) + 600) / 600 === event.target.children[0].children.length - 1) {
+                let elem = event.target.children[0].children[0].cloneNode()
+                event.target.children[0].appendChild(elem)
+                event.target.children[0].removeChild(event.target.children[0].children[0])
+                event.target.children[0].style.left = offset[id] + 'px'
             } else {
                 event.target.children[0].style.left = offset[id] - 600 + 'px'
                 offset[id] -= 600
             }
         } else if (mouseDown < clientX - (domRect.left + event.target.clientLeft)){
-            if (parseInt(offset[id]) === 0) {
-                event.target.children[0].style.left = -Math.abs(event.target.children[0].children.length * 600) + 600 + 'px'
-                offset[id] = -Math.abs(event.target.children[0].children.length * 600) + 600
-            } else {
-                event.target.children[0].style.left = offset[id] + 600 + 'px'
-                offset[id] += 600
-            }
+            event.target.children[0].style.left = offset[id] + 600 + 'px'
+            offset[id] += 600
         }
     }
     mouseDown = false
